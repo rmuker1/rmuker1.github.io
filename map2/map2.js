392265b181486c66c705e2e12f27afc440f03bca
RiaMukerji <rmuker1@lsu.com>
2020-04-14 17:03:50 -0500

    update

--- map2/map2.js
+++ map2/map2.js
@@ -1,7 +1,14 @@
+/* global L */
 var map2 = L.map('map2').setView([32.18, -99.14], 4)
 L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png').addTo(map2)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(map2)
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'warnings_c',
   format: 'image/png',
   transparent: true,
   attribution: 'NOAA, Iowa State University'
