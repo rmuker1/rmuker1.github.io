54fc166ad7d30034100e9d49e89f51c6d04eafdf
RiaMukerji <rmuker1@lsu.com>
2020-04-23 07:58:59 -0500

    update

--- map3/map3.js
+++ map3/map3.js
@@ -3,5 +3,25 @@ var map3 = L.map('map3').setView([32.18, -99.14], 4)
 L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png').addTo(map3)
 var statesUrl = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(statesUrl, function (data) {
-  L.geoJSON(data, { style: { color: 'pink' } }).addTo(map3)
+  var stateStyle = function (feature) {
+    var pop = feature.properties.POPULATION
+    var stateColor = 'violet'
+    if (pop < 4631918.5) { stateColor = 'darkmagenta' }
+    return {
+      color: stateColor,
+      weight: 1.5,
+      fillOpacity: 0.2
+    }
+  }
+  var renameThisGeojsonOptionsObject = {
+  style: stateStyle,
+  onEachFeature: createPopup
+  }
+}
+  L.geoJSON(data, GeojsonOptions).addTo(map3)
 })
+var createPopup = function (feature, layer) {
+  var name = feature.properties.STATE_NAME
+  var age = feature.properties.POPULATION
+  layer.bindPopup('Population of ' + name + ': ' + age + '<br>National average: 4631918.5')
+}
